b3e7dcd0b73a0ebc1c83a5e5ff02acef10341534
kubilay <kubidik27@gmail.com>
2022-01-22 03:00:41 +0300

    prepare tanımı kullanıldı

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import {useState} from 'react';
 
 import { useDispatch } from 'react-redux';
 import {addTodo} from "../redux/todos/todosSlice"
-import {nanoid} from "@reduxjs/toolkit"
+
 
 function Form() {
     const[title,setTitle] =useState("")
@@ -16,7 +16,7 @@ function Form() {
         }
         e.preventDefault()
 
-        dispatch(addTodo({id: nanoid(), title, completed:false }))
+        dispatch(addTodo({title}))
 
         setTitle("")
     }
